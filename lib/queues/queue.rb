f937e00604d52a03d3facf8a68ebc0d5a67e3b36
Paulo Rodrigues <paulor2222@gmail.com>
2025-04-05 15:43:57 -0300

    queue(array) impl

--- lib/queues/queue.rb
+++ lib/queues/queue.rb
@@ -1,2 +1,38 @@
-class Queue
+class EmptyQueue < StandardError
+  def initialize(msg = 'Queue is empty')
+    super
+    @msg = msg
+  end
+end
+
+class MyQueue
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  def enqueue(item)
+    items << item
+  end
+
+  def dequeue
+    raise EmptyQueue if items.empty?
+
+    items.shift
+  end
+
+  def peek
+    raise EmptyQueue if items.empty?
+
+    items.first
+  end
+
+  def empty?
+    items.empty?
+  end
+
+  def size
+    items.size
+  end
 end
